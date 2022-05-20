f614d227ca19a53b395ff721e55975134e0ae02d
Irina <101044906+Liora377@users.noreply.github.com>
2022-05-20 14:48:51 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5363387358:AAGjU99pS7CEMlW4gWTyaB_C3vPzlRCnYQs'
+
+keys = {'рубль': 'RUB',
+        'евро': 'EUR',
+        'доллар': 'USD',
+        'юань': 'CNY',
+        'йена': 'JPY',
+        'биткоин':'BTC',
+        'эфириум': 'ETH'
+}
